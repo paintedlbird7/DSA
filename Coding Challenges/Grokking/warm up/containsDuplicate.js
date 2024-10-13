b9bb1b3bc6dcd2c6faa4caddffbb5d5d2f2454e1
Rosa M Perez <rosaperezlarios@gmail.com>
2024-10-12 17:57:30 -0700

    added Grokking DSA

---
+++ Coding Challenges/Grokking/warm up/containsDuplicate.js
@@ -0,0 +1,12 @@
+function containsDuplicate(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
