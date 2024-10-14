7451ebd5c8e350b5ceb170c2f18c9d05474c99a4
Rosa M Perez <rosaperezlarios@gmail.com>
2024-10-14 16:36:33 -0700

    added comments

--- Coding Challenges/Grokking/warm up/containsDuplicate.js
+++ Coding Challenges/Grokking/warm up/containsDuplicate.js
@@ -1,12 +1,12 @@
 function containsDuplicate(nums) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
+            if (nums[i] === nums[j]) { // if any two elements are the same, return true
                 return true;
             }
         }
     }
-    return false;
+    return false;  // if no duplicates are found, return false
 }
 
 console.log(containsDuplicate([1, 2, 3, 1]));
--- Coding Challenges/Grokking/warm up/isPangram.js
+++ Coding Challenges/Grokking/warm up/isPangram.js
@@ -1,12 +1,12 @@
-function checkIfPangram(sentence) {
-    const seen = new Set();
-    for(let i=0; i<sentence.length; i++) {
-      const currChar = sentence[i].toLowerCase();
+function checkIfPangram(sentence) {   // Function to check if given sentence is pangram
+    const seen = new Set();     // Create a set to store unique characters
+    for(let i=0; i<sentence.length; i++) {     // Iterate over each character using a normal for loop
+      const currChar = sentence[i].toLowerCase();       // Convert the current character to lowercase
       if (/[a-z]/.test(currChar)){
-        seen.add(currChar);
+        seen.add(currChar);    // Add the character to the set
       }
     }
-    return seen.size === 26;
+    return seen.size === 26; // Return true if set size is 26 (total number of alphabets)
   }
   
   console.log(checkIfPangram("TheQuickBrownFoxJumpsOverTheLazyDog"));
