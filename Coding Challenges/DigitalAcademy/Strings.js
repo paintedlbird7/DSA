cd44b3de042b7a009d4c9320d682860dd16da8ba
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-12 11:16:47 -0800

    string concat

---
+++ Coding Challenges/DigitalAcademy/Strings.js
@@ -0,0 +1,37 @@
+let name = "Rosa"
+name += " Perez"
+console.log(name)
+
+let str1 = "Hello"
+let str2 = " World"
+let result =str1.concat('', str2)
+console.log(result)
+
+let greeting = "hello";
+console.log(greeting[4]);
+
+let firstTwo = greeting[0]+greeting[1];
+console.log(firstTwo);    
+
+console.log(greeting[greeting.length-1]);
+
+let poem = "Roses are Red\nViolets are blue\nJavascript is fun\nand so are you"
+console.log(poem)
+
+let statement = "She said,\"Hello!\"";
+console.log(statement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
