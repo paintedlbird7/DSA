1bdb5174776caffaf3e54a57913bef1d9ba32ccd
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-18 18:27:03 -0800

    Functions

---
+++ Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/2 Functions.js
@@ -0,0 +1,24 @@
+// Functions                      11-16-2025
+function mystery(a, b = 3) {
+    return a * b;
+  }
+  console.log(mystery(4)); // 12
+  
+  function greet(){
+   console.log("Hello Rosa")
+  }
+  greet();
+  
+  function greet2(name){
+   console.log("Hello "+name+"!")
+  }
+   greet2("Maria");
+  
+  
+  // Anonymous Function
+  const sum = function(num1, num2) {
+   return num1 + num2;
+  }
+  console.log(sum(1,2));
+  
+  // Arrow Function  
