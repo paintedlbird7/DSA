614342fdbf2e147293587aeb9232e6a002b73923
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-21 15:19:58 -0800

    Objects & Properties

--- Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/4 Objects.js
+++ Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/4 Objects.js
@@ -1,2 +1,25 @@
 // Objects
-
+// Properties of an object
+const person = {
+    name: "Alice",
+    age: 36,
+   //  city: "New York",
+    "property with spaces": "World", 
+    addresses: [
+     { type: "home", street: "123 Main St", city: "Anytown"},
+     { type: "work", street: "456 Market St", city: "Workville" },
+    ],
+    contact: {
+     email: "alice@example.com",
+     phone: {
+      home: "4081234567",
+      work: "309 877 3455",
+        
+      }
+    }
+   }
+   console.log(person.name)
+   console.log(person.age)
+   console.log(person["property with spaces"])
+   console.log(person.contact.phone.work);
+   console.log(person.addresses[1].city);   
