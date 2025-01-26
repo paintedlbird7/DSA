20ab1f134e1e0451aced0617c21492e08dbdcfca
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-25 17:10:41 -0800

    Review

---
+++ Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/6 Review.js
@@ -0,0 +1,11 @@
+// Review
+
+let num = 5.7;
+console.log(num.toString()); // "5.7"
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.toString()); // "1,2,3,4,5"
+
+let obj = { name: "John", age: 30 };
+console.log(obj.toString()); // "[object Object]"
+
