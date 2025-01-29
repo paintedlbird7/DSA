ce07c1b0af82f33fb3226bc0f2e8bd9033dd7443
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-29 07:43:20 -0800

    added gitignore

--- Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/6 Review.js
+++ Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/6 Review.js
@@ -9,5 +9,6 @@ console.log(arr.toString()); // "1,2,3,4,5"
 let obj = { name: "John", age: 30 };
 console.log(obj.toString()); // "[object Object]"
 
-
+const fruits = ["apple", "banana", "orange"]
+console.log(fruits.length) // 3
 
