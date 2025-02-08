08a9d11d4a52723cf517b7c9ac42301dd5862556
Rosa M Perez <rosaperezlarios@gmail.com>
2025-02-08 08:12:03 -0800

    adding Closures, Hoisting

--- Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/Higher Order Functions & Callbacks.js
+++ Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/Higher Order Functions & Callbacks.js
@@ -1,1 +1,22 @@
 // Higher Order Functions & Callbacks
+
+// Hoisting
+sayHello(); // Hello World
+function sayHello(){
+ console.log("Hello world");
+}
+
+// Closures
+// Closure
+function outerFunction(x) {
+    let y = 10;
+    function innerFunction() {
+     console.log(x+y)
+    }
+    return innerFunction;
+   }
+   let closure = outerFunction(5);
+   closure() // 15
+
+
+
