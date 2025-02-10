351167c2a939728bd663112ce96dcdb1233fc917
Rosa M Perez <rosaperezlarios@gmail.com>
2025-02-09 19:39:36 -0800

    definition of hoisting

--- Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/Higher Order Functions & Callbacks.js
+++ Coding Challenges/DigitalAcademy/Free Code Camp Full Stack/Higher Order Functions & Callbacks.js
@@ -1,13 +1,15 @@
 // Higher Order Functions & Callbacks
 
 // Hoisting
+//Hoisting in JavaScript is a behavior where variable and function declarations are moved 
+// to the top of their scope before code execution
+//eclarations are hoisted, not initializations
 sayHello(); // Hello World
 function sayHello(){
  console.log("Hello world");
 }
 
 // Closures
-// Closure
 function outerFunction(x) {
     let y = 10;
     function innerFunction() {
